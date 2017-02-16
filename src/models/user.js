a5eeeb0efce0b5e3b5948f45f84306da181e2a23
FS-TimothyMcCoy <tjfatterstacks@live.com>
2017-02-15 23:49:23 -0500

    Add Create and Read CRUD functionality

--- src/models/user.js
+++ src/models/user.js
@@ -3,3 +3,7 @@ const db = require('./db');
 exports.create = (payload, err, success) => {
   db.user.create(payload).then(success).catch(err);
 }
+
+exports.findAll = (err, success) => {
+  db.user.findAll().then(success).catch(err);
+}
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,25 +1,25 @@
 const user = require('../../models/user')
 
 module.exports = (express) => {
-    const router = express.Router();
+const router = express.Router();
 
-    router.get('/users', (req, res) => {
-        res.json({
-            healthy: true,
+router.get('/users', (req, res) => {
+    user.findAll( (err) => {
+        res.status(500).json(err);
+    }, (data) => {
+        res.status(200).json(data);
+    })
+  });
 
-        })
+//Create
+router.post('/users', (req, res) => {
+    user.create(req.body, (err) => {
+        res.status(500).json(err);
+    }, (data) => {
+        res.status(200).json(data);
+    })
+});
 
-    });
-
-    //Create
-    router.post('/users', (req, res) => {
-        user.create(req.body, (err) => {
-            res.status(500).json(err);
-        }), (data) => {
-            res.status(200).json(data);
-        }
-    });
-
-    return router;
+return router;
 
 }
